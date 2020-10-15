d14cdae92791957f00e5c43d425d813638f7b406
hudandan <hudandan@cygmgs.com>
2020-10-15 16:49:01 +0800

    修改登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -74,8 +74,8 @@ export default {
     }
     // 密码校验
     const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error('密码不能小于6位'))
+      if (value.length < 4) {
+        callback(new Error('密码不能小于4位'))
       } else {
         callback()
       }
